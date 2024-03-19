983c66a6940171fe0ff2d245911bef9a20cc20de
b4rtm <bartek.matusiak12@gmail.com>
2024-03-19 17:58:39 +0100

    added pesel

--- src/components/pages/RegisterPage.jsx
+++ src/components/pages/RegisterPage.jsx
@@ -16,6 +16,7 @@ const RegisterPage = () =>{
     const [phoneNumber, setTel] = useState('');
     const [address, setAddress] = useState('');
     const [city, setCity] = useState('');
+    const [pesel, setPesel] = useState('');
     const [postCode, setPostcode] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
@@ -40,6 +41,7 @@ const RegisterPage = () =>{
             address,
             city,
             postCode,
+            pesel,
             password
           });
           navigateTo('/login')
@@ -63,6 +65,7 @@ const RegisterPage = () =>{
                 <FormField label="address" name="Adres" type="text" onChange={(e) => setAddress(e.target.value)}/>
                 <FormField label="city" name="Miasto" type="text" onChange={(e) => setCity(e.target.value)} />
                 <FormField label="postcode" name="Kod pocztowy" type="text" onChange={(e) => setPostcode(e.target.value)}/>
+                <FormField label="pesel" name="PESEL" type="text" onChange={(e) => setPesel(e.target.value)}/>
                 <FormField label="password" name="Hasło" type="password" onChange={(e) => setPassword(e.target.value)}/>
                 <FormField label="confirm_password" name="Potwierdź hasło" type="password" onChange={(e) => setConfirmPassword(e.target.value)}/>
                 {!passwordsMatch && <p>Hasła nie są zgodne</p>}
