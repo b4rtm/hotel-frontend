4ffdcc2527559d5b775df1e98ed6fa5b7ef52c03
b4rtm <bartek.matusiak12@gmail.com>
2024-03-20 19:49:23 +0100

    added login validation

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -3,33 +3,54 @@ import '../../stylesheets/register-page.css'
 import Footer from '../Footer';
 import FormField from '../FormField';
 import Navbar from '../Navbar';
-const RegisterPage = () =>{
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
+
+const LoginPage = () =>{
 
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
-    const handleLogin = async () => {
-        try {
-            const response = await axios.post("http://localhost:8080/login", {
-              email,
-              password
-            });
-            navigateTo('/')
-          }
-          catch (error) {
-              console.error("login error:", error);
-          }
-    }
+
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+        },
+        validationSchema: Yup.object({
+            email: Yup.string().required('Wpisz email'),
+            password: Yup.string().required('Wpisz hasło'),
+        }),
+        onSubmit: async (values) => {
+
+            try {
+                const response = await axios.post("http://localhost:8080/login", {
+                  email,
+                  password
+                });
+                navigateTo('/')
+              }
+              catch (error) {
+                  console.error("login error:", error);
+              }
+        },
+    });
+
 
     return (
         <>
         <Navbar/>
         <div className='register-page'>
             <h1>Zaloguj się!</h1>
-            <form>
-                <FormField label="email" name="E-mail" type="text" onChange={(e) => setEmail(e.target.value)}/>
-                <FormField label="password" name="Hasło" type="password" onChange={(e) => setPassword(e.target.value)}/>
-                <button type="submit" onClick={handleLogin}>Zaloguj się</button>
+            <form onSubmit={formik.handleSubmit}>
+                <FormField label="email" name="Email" type="text" onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.email} />
+                {formik.touched.email && formik.errors.email && <p className="error">{formik.errors.email}</p>}
+                <FormField label="password" name="Hasło" type="password" onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.password} />
+                {formik.touched.password && formik.errors.password && <p className="error">{formik.errors.password}</p>}
+                {!formik.isValidating && <button type="submit">Zaloguj się</button>}                
+                {!formik.isValid && formik.submitCount > 0 && <p className="error">Wypełnij formularz logowania</p>}
             </form>
         </div>
         <Footer/>
@@ -37,4 +58,4 @@ const RegisterPage = () =>{
     );
 }
 
-export default RegisterPage;
+export default LoginPage;
