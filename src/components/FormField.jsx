9223b53dbf9582a9fcbc06e9ed1dea3edf3b4eec
b4rtm <bartek.matusiak12@gmail.com>
2024-03-24 17:12:50 +0100

    updating users

--- src/components/FormField.jsx
+++ src/components/FormField.jsx
@@ -1,8 +1,8 @@
 const FormField = ({label, name, type, onChange, onBlur, value}) =>  {
     return (
-        <div className="form-field" onChange={onChange} onBlur={onBlur} value={value}>
+        <div className="form-field" >
             <label htmlFor={label}>{name}</label>
-            <input name={label} id={label} type={type} />
+            <input name={label} id={label} type={type} onChange={onChange} onBlur={onBlur} value={value}/>
         </div>
     );
 }
