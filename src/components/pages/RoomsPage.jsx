6badd9285d9ae308fde043ecbde0e652247e34fa
b4rtm <bartek.matusiak12@gmail.com>
2024-08-05 13:21:36 +0200

    sorting out in alphabetical order

--- src/components/pages/RoomsPage.jsx
+++ src/components/pages/RoomsPage.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from 'react-router-dom';
 import { fetchRooms } from '../../api/rooms';
 import DatePicker from 'react-datepicker';
 import { Slider } from '@mui/material';
-import {isRoomAvailable } from '../../api/date';
+import { isRoomAvailable } from '../../api/date';
 
 const RoomsPage = () => {
     const [rooms, setRooms] = useState([]);
@@ -16,8 +16,8 @@ const RoomsPage = () => {
     const [capacity, setCapacity] = useState('');
     const [startDate, setStartDate] = useState(null);
     const [endDate, setEndDate] = useState(null);
+    const [sortOrder, setSortOrder] = useState(''); 
     const today = new Date();
-
 
     useEffect(() => {
         const fetchData = async () => {
@@ -38,13 +38,15 @@ const RoomsPage = () => {
 
     const handlePriceRangeChange = (event, newValue) => {
         setPriceRange(newValue);
-        console.log(rooms);
     };
 
     const handleCapacityChange = (event) => {
         setCapacity(event.target.value);
     };
 
+    const handleSortChange = (event) => {
+        setSortOrder(event.target.value);
+    };
 
     const filteredRooms = rooms.filter(room => {
         const matchesSearchTerm = room.name.toLowerCase().includes(searchTerm.toLowerCase());
@@ -54,7 +56,17 @@ const RoomsPage = () => {
         const matchesAvailability = isRoomAvailable(room, startDate, endDate);
 
         return matchesSearchTerm && matchesMinPrice && matchesMaxPrice && matchesCapacity && matchesAvailability;
-        });
+    });
+
+    const sortedRooms = filteredRooms.sort((a, b) => {
+        if (sortOrder === 'asc') {
+            return a.name.localeCompare(b.name);
+        } else if (sortOrder === 'desc') {
+            return b.name.localeCompare(a.name);
+        } else {
+            return 0;
+        }
+    });
 
     const getAriaValueText = (value) => {
         return `${value} zł`;
@@ -63,7 +75,7 @@ const RoomsPage = () => {
     const roomsPerPage = 6;
     const indexOfLastRoom = currentPage * roomsPerPage;
     const indexOfFirstRoom = indexOfLastRoom - roomsPerPage;
-    const currentRooms = filteredRooms.slice(indexOfFirstRoom, indexOfLastRoom);
+    const currentRooms = sortedRooms.slice(indexOfFirstRoom, indexOfLastRoom);
 
     const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
@@ -75,6 +87,14 @@ const RoomsPage = () => {
                 <div className='main-content'>
                     <div className="filter-panel">
                         <input className="search-input" type="text" placeholder="Wyszukaj pokój..." value={searchTerm} onChange={handleSearchChange} />
+                        <label>
+                            Sortuj według:
+                            <select value={sortOrder} onChange={handleSortChange}>
+                                <option value=''>Wybierz opcję</option>
+                                <option value='asc'>od A do Z</option>
+                                <option value='desc'>od Z do A</option>
+                            </select>
+                        </label>
                         <label>
                             Zakres cen: {priceRange[0]} zł - {priceRange[1]} zł
                             <Slider
@@ -99,35 +119,35 @@ const RoomsPage = () => {
                             </select>
                         </label>
                         <div className='date'>
-                        <div className='pick-date'>
-                            <label>Początek rezerwacji:</label>
-                            <DatePicker
-                            selected={startDate}
-                            onChange={(date) => setStartDate(date)}
-                            selectsStart
-                            startDate={startDate}
-                            endDate={endDate}
-                            minDate={today} 
-                            dateFormat="dd/MM/yyyy"
-                            placeholderText="Wybierz datę początku"
-                            locale="pl"
-                            />
+                            <div className='pick-date'>
+                                <label>Początek rezerwacji:</label>
+                                <DatePicker
+                                    selected={startDate}
+                                    onChange={(date) => setStartDate(date)}
+                                    selectsStart
+                                    startDate={startDate}
+                                    endDate={endDate}
+                                    minDate={today}
+                                    dateFormat="dd/MM/yyyy"
+                                    placeholderText="Wybierz datę początku"
+                                    locale="pl"
+                                />
+                            </div>
+                            <div className='pick-date'>
+                                <label>Koniec rezerwacji:</label>
+                                <DatePicker
+                                    selected={endDate}
+                                    onChange={(date) => setEndDate(date)}
+                                    selectsEnd
+                                    startDate={startDate}
+                                    endDate={endDate}
+                                    minDate={startDate}
+                                    dateFormat="dd/MM/yyyy"
+                                    placeholderText="Wybierz datę końca"
+                                    locale="pl"
+                                />
+                            </div>
                         </div>
-                        <div className='pick-date'>
-                            <label>Koniec rezerwacji:</label>
-                            <DatePicker
-                            selected={endDate}
-                            onChange={(date) => setEndDate(date)}
-                            selectsEnd
-                            startDate={startDate}
-                            endDate={endDate}
-                            minDate={startDate}
-                            dateFormat="dd/MM/yyyy"
-                            placeholderText="Wybierz datę końca"
-                            locale="pl"
-                            />
-                        </div>
-                    </div>
                     </div>
                     <div className='rooms'>
                         <div className='rooms-list'>
@@ -141,7 +161,7 @@ const RoomsPage = () => {
                             ))}
                         </div>
                         <div className="pagination">
-                            {Array.from({ length: Math.ceil(filteredRooms.length / roomsPerPage) }, (_, i) => (
+                            {Array.from({ length: Math.ceil(sortedRooms.length / roomsPerPage) }, (_, i) => (
                                 <div key={i} className={currentPage === i + 1 ? 'active' : ''} onClick={() => paginate(i + 1)}>{i + 1}</div>
                             ))}
                         </div>
