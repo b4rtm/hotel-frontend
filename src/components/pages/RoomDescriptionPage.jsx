ecb78ca04a05957b409c98a547561eedde14852e
b4rtm <bartek.matusiak12@gmail.com>
2024-08-25 14:33:37 +0200

    fix dates while booking room

--- src/components/pages/RoomDescriptionPage.jsx
+++ src/components/pages/RoomDescriptionPage.jsx
@@ -159,9 +159,17 @@ const RoomDescriptionPage = () => {
                         <button onClick={closeModal} style={{ backgroundColor: 'darkred', border: 'red' }}>{t('cancel')}</button>
                         <button onClick={async () => {
                             closeModal();
+                            const startDateWithAddedDay = new Date(startDate);
+                            startDateWithAddedDay.setDate(startDateWithAddedDay.getDate() + 1);
+                            const formattedStartDate = startDateWithAddedDay.toISOString().split('T')[0];
+
+                            const endDateWithAddedDay = new Date(endDate);
+                            endDateWithAddedDay.setDate(endDateWithAddedDay.getDate() + 1);
+                            const formattedEndDate = endDateWithAddedDay.toISOString().split('T')[0];
+
                             const id = await postBooking({
-                                checkInDate: startDate,
-                                checkOutDate: endDate,
+                                checkInDate:  formattedStartDate,
+                                checkOutDate: formattedEndDate,
                                 roomId: room.id,
                                 customerId: user?.id
                             });
