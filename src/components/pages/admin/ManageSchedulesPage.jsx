935efe270abe86135505eb7f49bc769c791a2d7a
b4rtm <bartek.matusiak12@gmail.com>
2024-09-03 21:11:06 +0200

    schedule translated to polish

--- src/components/pages/admin/ManageSchedulesPage.jsx
+++ src/components/pages/admin/ManageSchedulesPage.jsx
@@ -4,7 +4,6 @@ import Paper from '@mui/material/Paper';
 import {
   Scheduler,
   WeekView,
-  MonthView,
   DayView,
   Appointments,
   AppointmentForm,
@@ -14,97 +13,166 @@ import {
   ViewSwitcher,
 } from '@devexpress/dx-react-scheduler-material-ui';
 import { EditingState, IntegratedEditing, ViewState } from '@devexpress/dx-react-scheduler';
-import { plPL } from '@mui/x-date-pickers/locales';
-import { TextField, MenuItem } from '@mui/material';
+import { TextField, MenuItem, Select } from '@mui/material';
+import { fetchEmployees } from "../../../api/employees";
+import { DateTimePicker } from '@mui/x-date-pickers';
 
 const ManageSchedulesPage = () => {
 
 
-    const [data, setData] = useState([]);
-    const [employees, setEmployees] = useState([]);
-    const [currentDate, setCurrentDate] = useState(new Date());
-    const [currentViewName, setCurrentViewName] = useState('Week');
-  
-    useEffect(() => {
-      const fetchData = async () => {
+  const [data, setData] = useState([
+    {
+        id: 1,
+        title: 'Zmiana poranna - Recepcja',
+        startDate: new Date(2024, 8, 5, 9, 0), // 5 września 2024, 9:00
+        endDate: new Date(2024, 8, 5, 13, 0),  // 5 września 2024, 13:00
+        employeeId: 1,
+    },
+    {
+        id: 2,
+        title: 'Zmiana wieczorna - Ochrona',
+        startDate: new Date(2024, 8, 5, 18, 0), // 5 września 2024, 18:00
+        endDate: new Date(2024, 8, 5, 22, 0),  // 5 września 2024, 22:00
+        employeeId: 2,
+    },
+    {
+        id: 3,
+        title: 'Zmiana na lunch - Kucharz',
+        startDate: new Date(2024, 8, 6, 11, 0), // 6 września 2024, 11:00
+        endDate: new Date(2024, 8, 6, 15, 0),  // 6 września 2024, 15:00
+        employeeId: 3,
+    },
+  ]);
+  const [employees, setEmployees] = useState([]);
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const [currentViewName, setCurrentViewName] = useState('Week');
 
-      };
-  
-      fetchData();
-    }, []);
-  
-    const commitChanges = async ({ added, changed, deleted }) => {
-      if (added) {
-
-      }
-      if (changed) {
-
-      }
-      if (deleted !== undefined) {
-
-      }
+  useEffect(() => {
+    const fetchData = async () => {
+      const employeesData = await fetchEmployees();
+      setEmployees(employeesData);
     };
-  
-    const EmployeeSelector = ({ appointmentData, onFieldChange }) => {
-      const currentEmployeeId = appointmentData.employeeId || '';
-  
-      return (
-        <TextField
-          select
-          label="Select Employee"
-          value={currentEmployeeId}
-          onChange={(e) => onFieldChange({ employeeId: e.target.value })}
-          fullWidth
-        >
-          {employees.map((employee) => (
-            <MenuItem key={employee.id} value={employee.id}>
-              {employee.name}
-            </MenuItem>
-          ))}
-        </TextField>
-      );
-    };
-  
-    const BasicLayout = ({ appointmentData, onFieldChange, ...restProps }) => (
-      <AppointmentForm.BasicLayout
-        appointmentData={appointmentData}
-        onFieldChange={onFieldChange}
-        {...restProps}
-      >
-        <EmployeeSelector appointmentData={appointmentData} onFieldChange={onFieldChange} />
-      </AppointmentForm.BasicLayout>
-    );
-  
-    const CustomAppointmentForm = (props) => (
-      <AppointmentForm
-        {...props}
-        basicLayoutComponent={BasicLayout}
-      />
-    );
-  
+
+    fetchData();
+  }, []);
+
+  const handleViewChange = (viewName) => {
+    setCurrentViewName(viewName);
+  }
+
+  const commitChanges = async ({ added, changed, deleted }) => {
+    if (added) {
+      // Obsługa dodawania
+      console.log('Dodano nowe wydarzenie:', added);
+    }
+    if (changed) {
+      // Obsługa zmieniania
+      console.log('Zmodyfikowano wydarzenie:', changed);
+    }
+    if (deleted !== undefined) {
+      // Obsługa usuwania
+      console.log('Usunięto wydarzenie o ID:', deleted);
+    }
+  };
+
+  const EmployeeSelector = ({ appointmentData, onFieldChange }) => {
+    const currentEmployeeId = appointmentData.employeeId || '';
+
     return (
-      <Paper>
-        <Scheduler data={data} locale={plPL}>
-          <ViewState
-            currentDate={currentDate}
-            onCurrentDateChange={setCurrentDate}
-            currentViewName={currentViewName}
-            onCurrentViewNameChange={setCurrentViewName}
-          />
-          <EditingState onCommitChanges={commitChanges} />
-          <IntegratedEditing />
-          <DayView startDayHour={9} endDayHour={17} />
-          <WeekView startDayHour={9} endDayHour={17} />
-          <MonthView />
-          <Appointments />
-          <AppointmentTooltip showOpenButton showDeleteButton />
-          <CustomAppointmentForm />
-          <Toolbar />
-          <DateNavigator />
-          <ViewSwitcher />
-        </Scheduler>
-      </Paper>
+      <TextField
+        select
+        label="Wybierz pracownika"
+        value={currentEmployeeId}
+        onChange={(e) => onFieldChange({ employeeId: e.target.value })}
+        fullWidth
+      >
+        {employees.map((employee) => (
+          <MenuItem key={employee.id} value={employee.id}>
+            {employee.name}
+          </MenuItem>
+        ))}
+      </TextField>
     );
   };
+
+  const BasicLayout = ({ appointmentData, onFieldChange, ...restProps }) => (
+    <AppointmentForm.BasicLayout
+    appointmentData={appointmentData}
+    onFieldChange={onFieldChange}
+    {...restProps}
+  >
+    <EmployeeSelector appointmentData={appointmentData} onFieldChange={onFieldChange} />
+  </AppointmentForm.BasicLayout>
+  );
+
+  const CustomViewSwitcher = ({ currentViewName, onChange }) => (
+    <Select
+      value={currentViewName}
+      onChange={(e) => onChange(e.target.value)}
+      variant="outlined"
+      displayEmpty
+    >
+      <MenuItem value="Day">Dzień</MenuItem>
+      <MenuItem value="Week">Tydzień</MenuItem>
+    </Select>
+  );
+
+  const CustomAppointmentForm = (props) => (
+    <AppointmentForm
+      {...props}
+      basicLayoutComponent={BasicLayout}
+      messages={{
+        moreInformationLabel: 'Notatki',
+        commitCommand: 'Zapisz', // Zmiana tekstu przycisku 'Save' na 'Zapisz'
+        detailsLabel: 'Szczegóły',
+        allDayLabel: 'Cały dzień',
+        repeatLabel: 'Powtarzaj',
+        titleLabel: 'Tytuł',
+        frequencyLabel: 'Częstotliwość', // Nowe tłumaczenia dla opcji powtarzania
+        dailyLabel: 'Codziennie',
+        weeklyLabel: 'Co tydzień',
+        monthlyLabel: 'Co miesiąc',
+        yearlyLabel: 'Co rok',
+        repeatEveryLabel: 'Powtarzaj co',
+        daysLabel: 'dni',
+        endRepeatLabel: 'Zakończ powtarzanie',
+        neverLabel: 'Nigdy',
+        onLabel: 'Po',
+        occurrencesLabel: 'razach',
+        afterLabel: 'Po',
+        repeatOnLabel: 'Powtarzaj w',
+        monthsLabel: 'miesiące',
+        ofEveryMonthLabel: 'każdego miesiąca',
+        ofLabel: 'z',
+        startOnLabel: 'Rozpocznij w',
+      }}
+    />
+  );
+  return (
+    <Paper>
+      <Scheduler data={data} locale="pl">
+        <ViewState
+          currentDate={currentDate}
+          onCurrentDateChange={setCurrentDate}
+          currentViewName={currentViewName}
+          onCurrentViewNameChange={handleViewChange}
+        />
+        <EditingState onCommitChanges={commitChanges} />
+        <IntegratedEditing />
+        <DayView startDayHour={9} endDayHour={22} />
+        <WeekView startDayHour={9} endDayHour={22} />
+        <Appointments />
+        <AppointmentTooltip showOpenButton showDeleteButton />
+        <CustomAppointmentForm />
+        <Toolbar />
+        <DateNavigator />
+        <CustomViewSwitcher
+          currentViewName={currentViewName}
+          onChange={handleViewChange} 
+        />
+        </Scheduler>
+    </Paper>
+  );
+};
 
 export default ManageSchedulesPage;
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import { LanguageProvider } from './translations/LanguageContext.jsx'
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import pl from 'date-fns/locale/pl';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <GoogleOAuthProvider clientId='8218279479-970tm6tra2pje83ob0mlqn2a8hepme94.apps.googleusercontent.com'>
     <React.StrictMode>
       <LanguageProvider>
+      <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={pl}>
+
       <App />
+      </LocalizationProvider>,
+
       </LanguageProvider>
     </React.StrictMode>
   </GoogleOAuthProvider>
