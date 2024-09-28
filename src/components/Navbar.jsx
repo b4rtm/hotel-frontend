cfd6c041498444eabced0ddfa781cc3f3cb615e8
b4rtm <bartek.matusiak12@gmail.com>
2024-09-28 19:06:00 +0200

    move translations to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,83 +1,90 @@
-import '../stylesheets/navbar.css'
-import { Link } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
-import { useEffect, useState } from 'react';
-import i18next from 'i18next';
-import '../translations/transations'
-import { fetchUser } from '../api/users';
-import Dropdown from 'react-dropdown';
-import 'react-dropdown/style.css';
-import { useNavigate } from 'react-router-dom';
+import "../stylesheets/navbar.css";
+import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { useEffect, useState } from "react";
+import "../translations/transations";
+import { fetchUser } from "../api/users";
+import Dropdown from "react-dropdown";
+import "react-dropdown/style.css";
+import { useNavigate } from "react-router-dom";
 
-
-const Navbar = () =>{
-
-  const { t } = useTranslation();
+const Navbar = () => {
+  const { t, i18n } = useTranslation();
   const [user, setUser] = useState(null);
   const navigate = useNavigate();
 
+  const handleLanguageChange = (lang) => {
+    i18n.changeLanguage(lang); // Change the language here
+  };
+
   const options = [
-    { value: 'myReservations', label: t("myReservations") },
-    { value: 'logout', label: t("logout") }
+    { value: "myReservations", label: t("myReservations") },
+    { value: "logout", label: t("logout") },
   ];
 
   const handleDropdownChange = (option) => {
     switch (option.value) {
-      case 'myReservations':
-        navigate('/booking-history/' + user.id);
+      case "myReservations":
+        navigate("/booking-history/" + user.id);
         break;
-      case 'logout':
-        localStorage.removeItem('token')
-        navigate('/login');
+      case "logout":
+        localStorage.removeItem("token");
+        navigate("/login");
         break;
       default:
         break;
     }
   };
-  useEffect(() =>{
+  useEffect(() => {
     const fetchData = async () => {
       const userData = await fetchUser();
-      setUser(userData)
-    }
+      setUser(userData);
+    };
     fetchData();
-  }, [])
+  }, []);
 
   return (
     <div className="navbar">
-       <Link to="/">
-        <img className="logo" src="/logo.png" alt="Logo"/>
+      <Link to="/">
+        <img className="logo" src="/logo.png" alt="Logo" />
       </Link>
-      <Link to="/rooms" className='navbar-item'>
-        {t('rooms')}
+      <Link to="/rooms" className="navbar-item">
+        {t("rooms")}
       </Link>
-      <Link to="/contact" className='navbar-item'>
-        {t('contact')}
+      <Link to="/contact" className="navbar-item">
+        {t("contact")}
       </Link>
-      { user ? (
+      {user ? (
         <>
- <div className='navbar-item'>
-      <Dropdown
-      className='navbar-item'
-      options={options}
-      onChange={handleDropdownChange}
-      placeholder={`${user.name} ${user.surname}`}
-    />
-    </div>
+          <div className="navbar-item">
+            <Dropdown
+              className="navbar-item"
+              options={options}
+              onChange={handleDropdownChange}
+              placeholder={`${user.name} ${user.surname}`}
+            />
+          </div>
         </>
       ) : (
         <>
-          <Link to="/login" className='navbar-item'>
-            {t('login')}
+          <Link to="/login" className="navbar-item">
+            {t("login")}
           </Link>
-          <Link to="/register" className='navbar-item'>
-            {t('register')}
+          <Link to="/register" className="navbar-item">
+            {t("register")}
           </Link>
         </>
       )}
-      
+        <div className="select-lang">
+          <div className="lang-icon" onClick={() => handleLanguageChange('pl')}>
+            <img src="/poland-flag-icon.png" alt="PL" />
+          </div>
+          <div className="lang-icon" onClick={() => handleLanguageChange('en')}>
+            <img src="/uk-flag-icon.png" alt="EN" />
+          </div>
+        </div>
     </div>
   );
-    
-}
+};
 
 export default Navbar;
--- src/components/pages/MainPage.jsx
+++ src/components/pages/MainPage.jsx
@@ -1,39 +1,19 @@
-import '../../stylesheets/main-page.css';
-import Navbar from '../Navbar';
-import Footer from '../Footer';
-import { useEffect, useState } from 'react';
-import { useLanguage } from '../../translations/LanguageContext';
-import { useTranslation } from 'react-i18next';
+import "../../stylesheets/main-page.css";
+import Navbar from "../Navbar";
+import Footer from "../Footer";
+import { useTranslation } from "react-i18next";
 
 function MainPage() {
-  const { language, setLanguage } = useLanguage();
   const { t } = useTranslation();
-
-  const handleLanguageChange = (lang) => {
-    setLanguage(lang);
-  };
-
-  useEffect(() => {
-    const storedLanguage = localStorage.getItem('language') || 'pl';
-    setLanguage(storedLanguage);
-  }, [setLanguage]);
 
   return (
     <>
       <Navbar />
       <div className="main-page">
         <img src="lobby2.jpg" className="main-img" alt="Lobby" />
-        <div className="select-lang">
-          <div className="lang-icon" onClick={() => handleLanguageChange('pl')}>
-            <img src="/poland-flag-icon.png" alt="PL" />
-          </div>
-          <div className="lang-icon" onClick={() => handleLanguageChange('en')}>
-            <img src="/uk-flag-icon.png" alt="EN" />
-          </div>
-        </div>
       </div>
-      <div className='welcome'>
-        <span>{t('welcome')}</span>
+      <div className="welcome">
+        <span>{t("welcome")}</span>
         <span className="larger-text">Royal Residence</span>
         <span>Łódź</span>
       </div>
